b823fcff86b34796a562a004d7186ad4a9ee88c9
Esoteric Enderman <esotericenderman@gmail.com>
2025-04-17 18:39:09 +0100

    Clean up build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,42 +11,13 @@ plugins {
   id("io.github.goooler.shadow") version "8.1.7"
 }
 
-val groupStringSeparator = "."
-val kebabcaseStringSeparator = "-"
-val snakecaseStringSeparator = "_"
-
-fun capitaliseFirstLetter(string: String): String {
-  return string.first().uppercase() + string.slice(IntRange(1, string.length - 1))
-}
-
-fun snakecase(kebabcaseString: String): String {
-  return kebabcaseString.lowercase().replace(kebabcaseStringSeparator, snakecaseStringSeparator).replace(" ", snakecaseStringSeparator)
-}
-
-fun pascalcase(kebabcaseString: String): String {
-  var pascalCaseString = ""
-
-  val splitString = kebabcaseString.split(kebabcaseStringSeparator)
-
-  for (part in splitString) {
-    pascalCaseString += capitaliseFirstLetter(part)
-  }
-
-  return pascalCaseString
-}
-
 description = "A simple freeze wand plugin, which allows admins and staff to freeze players by right-clicking them with a custom stick item."
-
-val mainProjectAuthor = "Esoteric Enderman"
-val projectAuthors = listOfNotNull(mainProjectAuthor)
-
-val topLevelDomain = "dev"
 
 group = "dev.enderman"
 version = "1.0.0-SNAPSHOT"
 
 val javaVersion = 21
-val paperApiVersion = "1.21"
+val minecraftVersion = "1.21"
 
 java {
   toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)
@@ -57,7 +28,7 @@ repositories {
 }
 
 dependencies {
-  paperweight.paperDevBundle("$paperApiVersion-R0.1-SNAPSHOT")
+  paperweight.paperDevBundle("$minecraftVersion-R0.1-SNAPSHOT")
 
   implementation("dev.jorel" , "commandapi-bukkit-shade-mojang-mapped" , "9.5.1")
 }
@@ -76,10 +47,10 @@ bukkitPluginYaml {
   name = "FreezeWand"
   description = project.description
 
-  authors = projectAuthors
+  authors = listOf("Esoteric Enderman")
 
   version = project.version.toString()
-  apiVersion = paperApiVersion
+  apiVersion = minecraftVersion
   main = "$group.minecraft.plugins.freezewand.${name.get()}Plugin"
   load = BukkitPluginYaml.PluginLoadOrder.STARTUP
 }
